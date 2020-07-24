87b21a116f94a1604452e280f065f5b6c886d7e9
Javier Balloffet <javier.balloffet@gmail.com>
2020-07-23 23:01:54 -0300

    Add samples 9 to 12

---
+++ sample11/src/functions.c
@@ -0,0 +1,12 @@
+/*!
+ * @file   functions.c
+ * @brief  GNU Make - Sample code
+ * @author Javier Balloffet <javier.balloffet@gmail.com>
+ * @date   Jul 5, 2019
+ */
+#include <math.h>
+#include "functions.h"
+
+float get_square_root(int a) {
+  return sqrt(a);
+}
